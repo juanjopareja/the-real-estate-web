becf73c9ae29a5e5156f5cacf932a574bc19e920
juanjopareja <jupg25@msn.com>
2023-09-04 18:02:36 +0200

    Add dark mode button functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     eventListeners();
+    darkMode();
 });
+
+function darkMode() {
+    const darkModeButton = document.querySelector('.dark-mode-button');
+
+    darkModeButton.addEventListener('click', function() {
+        document.body.classList.toggle(`dark-mode`);
+    });
+}
 
 function eventListeners() {
     const mobileMenu = document.querySelector('.mobile-menu');
