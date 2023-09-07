a9fcf5a82e174c4ab6e206c1f9f303fe378e81d2
juanjopareja <jupg25@msn.com>
2023-09-07 18:42:54 +0200

    Add footer template php

--- advertisement.php
+++ advertisement.php
@@ -39,19 +39,6 @@
         </div>
     </main>
 
-    <footer class="footer section">
-        <div class="container footer-content">
-            <nav class="navigation">
-                <a href="about.html">Nosotros</a>
-                <a href="advertisements.html">Anuncios</a>
-                <a href="blog.html">Blog</a>
-                <a href="contact.html">Contacto</a>
-            </nav>
-        </div>
-
-        <p class="copyright">Todos los derechos reservados 2023 &copy;</p>
-    </footer>
-
-    <script src="build/js/bundle.min.js"></script>
-</body>
-</html>
+<?php
+    include './includes/templates/footer.php';
+?>
--- base.php
+++ base.php
@@ -6,19 +6,6 @@
         <h1>Título Página</h1>
     </main>
 
-    <footer class="footer section">
-        <div class="container footer-content">
-            <nav class="navigation">
-                <a href="about.html">Nosotros</a>
-                <a href="advertisements.html">Anuncios</a>
-                <a href="blog.html">Blog</a>
-                <a href="contact.html">Contacto</a>
-            </nav>
-        </div>
-
-        <p class="copyright">Todos los derechos reservados 2023 &copy;</p>
-    </footer>
-
-    <script src="build/js/bundle.min.js"></script>
-</body>
-</html>
+<?php
+    include './includes/templates/footer.php';
+?>
