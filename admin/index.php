ce064b03d5512249853a75639e3e7eaa51ac367a
juanjopareja <jupg25@msn.com>
2023-09-22 18:30:19 +0200

    Add properties table

--- admin/index.php
+++ admin/index.php
@@ -11,6 +11,31 @@
         <?php } ?>
 
         <a href="../admin/properties/create.php" class="button green-button">Nueva Propiedad</a>
+
+        <table class="properties">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Título</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Casa en la playa</td>
+                    <td><img src="/images/1111.jpg" class="table-image"></td>
+                    <td>1.200.000 €</td>
+                    <td>
+                        <a href="#" class="red-button-block">Eliminar</a>
+                        <a href="#" class="yellow-button-block">Actualizar</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </main>
 
 <?php
