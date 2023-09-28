1d28728f429599edb5eef681e2e2a521d987da25
juanjopareja <jupg25@msn.com>
2023-09-28 19:07:32 +0200

    Add password hash

--- user.php
+++ user.php
@@ -7,8 +7,10 @@ $db = connectDB();
 $email = "email@email.com";
 $password = "123456";
 
+$passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
 // query create user
-$query = "INSERT INTO users (email, password) VALUES ('$email', '$password')";
+$query = "INSERT INTO users (email, password) VALUES ('$email', '$passwordHash')";
 
 // add user to DB
 mysqli_query($db, $query);
