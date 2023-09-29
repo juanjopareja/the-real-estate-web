21528346732d0d1423a504e735e64cb04499d4d4
juanjopareja <jupg25@msn.com>
2023-09-29 19:39:10 +0200

    Add login form validation

--- login.php
+++ login.php
@@ -1,4 +1,24 @@
 <?php
+    require 'includes/config/database.php';
+    $db = connectDB();
+
+    // User authentication
+    $error = [];
+                
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $email = mysqli_real_escape_string($db, filter_var( $_POST['email'], FILTER_VALIDATE_EMAIL ));
+        $password = mysqli_real_escape_string($db, $_POST['password']);
+
+        if(!$email) {
+            $error[] = "El email es obligatorio o no es válido";
+        }
+
+        if(!$password) {
+            $error[] = "El password es obligatorio";
+        }
+    }
+
+    // Header include
     require 'includes/functions.php';
     includeTemplate('header');
 ?>
@@ -6,15 +26,22 @@
     <main class="container section center-content">
         <h1>Iniciar Sesión</h1>
 
-        <form action="" class="form">
+        <?php 
+            foreach($error as $e) { ?>
+                <div class="alert error">
+                    <?php echo $e; ?>
+                </div>
+        <?php } ?>
+
+        <form method="POST" class="form" novalidate>
         <fieldset>
                 <legend>Email y Password</legend>
                 
                 <label for="email">E-mail:</label>
-                <input type="email" placeholder="Tu Email" id="email">
+                <input type="email" name="email" placeholder="Tu Email" id="email" required>
 
                 <label for="password">Password:</label>
-                <input type="password" placeholder="Tu password" id="password">
+                <input type="password" name="password" placeholder="Tu password" id="password" required>
             </fieldset>
 
             <input type="submit" value="Iniciar Sesión" class="button green-button">
