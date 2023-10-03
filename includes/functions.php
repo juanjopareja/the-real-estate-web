f9a8883629cf3bc4bb4d221fed97c44bcb436d3d
juanjopareja <jupg25@msn.com>
2023-10-03 20:22:26 +0200

    Add autoload registering

--- includes/functions.php
+++ includes/functions.php
@@ -1,6 +1,7 @@
 <?php
 
-require 'app.php';
+define('TEMPLATES_URL', __DIR__ . '/templates');
+define('FUNCTIONS_URL', __DIR__ . 'functions.php');
 
 function includeTemplate(string $name, bool $start = false) {
     include TEMPLATES_URL . "/$name.php";
