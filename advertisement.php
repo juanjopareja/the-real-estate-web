efbec027c0ea288770bb65f221db3f9451809831
juanjopareja <jupg25@msn.com>
2023-10-09 22:34:20 +0200

    Add get and list properties

--- advertisement.php
+++ advertisement.php
@@ -1,4 +1,7 @@
 <?php
+    require 'includes/app.php';
+    use App\Property;
+
     $id = $_GET['id'];
     $id = filter_var($id, FILTER_VALIDATE_INT);
 
@@ -6,59 +9,41 @@
         header('Location: index.php');
     }
 
-    // Import Functions
-    require 'includes/app.php';
-
-    // DB connection import
-    $db = connectDB();
-
-    // Query
-    $query = "SELECT * FROM properties WHERE id = $id";
-    
-    // Get results
-    $result = mysqli_query($db, $query);
-
-    if(!$result -> num_rows) {
-        header('Location: index.php');
-    }
-
-    $property = mysqli_fetch_assoc($result);
+    $property = Property::find($id);
 
     includeTemplate('header');
 ?>
 
     <main class="container section center-content">
-        <h1><?php echo $property['title'] ?></h1>
+        <h1><?php echo $property->title ?></h1>
        
-        <img loading="lazy" src="images/<?php echo $property['image'] ?>" alt="imagen destacada">
+        <img loading="lazy" src="images/<?php echo $property->image ?>" alt="imagen destacada">
 
         <div class="property-summary">
-            <p class="price"><?php echo $property['price'] ?> €</p>
+            <p class="price"><?php echo $property->price ?> €</p>
 
             <ul class="icons-especifications icons-align">
                 <li>
                     <img class="icon" loading="lazy" src="build/img/icon_wc.svg" alt="icono wc">
-                    <p><?php echo $property['wc'] ?></p>
+                    <p><?php echo $property->wc ?></p>
                 </li>
 
                 <li>
                     <img class="icon" loading="lazy" src="build/img/icon_parking.svg" alt="icono parking">
-                    <p><?php echo $property['parking'] ?></p>
+                    <p><?php echo $property->parking ?></p>
                 </li>
 
                 <li>
                     <img class="icon" loading="lazy" src="build/img/icon_bedroom.svg" alt="icono dormitorio">
-                    <p><?php echo $property['bedrooms'] ?></p>
+                    <p><?php echo $property->bedrooms ?></p>
                 </li>
             </ul>
 
-            <p><?php echo $property['description'] ?></p>
+            <p><?php echo $property->description ?></p>
         </div>
     </main>
 
 <?php
-    // Close connection
-    mysqli_close($db);
 
     includeTemplate('footer');
 ?>
--- includes/templates/advertisements.php
+++ includes/templates/advertisements.php
@@ -1,50 +1,40 @@
 <?php 
-    // DB connection import
-    $db = connectDB();
+    use App\Property;
 
-    // Query
-    $query = "SELECT * FROM properties LIMIT $limit";
-    
-    // Get results
-    $result = mysqli_query($db, $query);
+    $properties = Property::all();    
 ?>
 
 <div class="container-advertisements">
-    <?php while($property = mysqli_fetch_assoc($result)) { ?>
+    <?php foreach($properties as $property) { ?>
 
     <div class="advertisement">
-        <img loading="lazy" src="images/<?php echo $property['image']; ?>" alt="imagen anuncio">
+        <img loading="lazy" src="images/<?php echo $property->image; ?>" alt="imagen anuncio">
         
         <div class="advertisement-content">
-            <h3><?php echo $property['title']; ?></h3>
-            <p><?php echo $property['description']; ?></p>
-            <p class="price"><?php echo $property['price']; ?> €</p>
+            <h3><?php echo $property->title; ?></h3>
+            <p><?php echo $property->description; ?></p>
+            <p class="price"><?php echo $property->price; ?> €</p>
 
             <ul class="icons-especifications">
                 <li>
                     <img class="icon" loading="lazy" src="build/img/icon_wc.svg" alt="icono wc">
-                    <p><?php echo $property['wc']; ?></p>
+                    <p><?php echo $property->wc; ?></p>
                 </li>
 
                 <li>
                     <img class="icon" loading="lazy" src="build/img/icon_parking.svg" alt="icono parking">
-                    <p><?php echo $property['parking']; ?></p>
+                    <p><?php echo $property->parking; ?></p>
                 </li>
 
                 <li>
                     <img class="icon" loading="lazy" src="build/img/icon_bedroom.svg" alt="icono dormitorio">
-                    <p><?php echo $property['bedrooms']; ?></p>
+                    <p><?php echo $property->bedrooms; ?></p>
                 </li>
             </ul>
 
-            <a href="advertisement.php?id=<?php echo $property['id']; ?>" class="yellow-button-block">Ver Propiedad</a>
+            <a href="advertisement.php?id=<?php echo $property->id; ?>" class="yellow-button-block">Ver Propiedad</a>
         </div><!-- .advertisements-content-->
     </div><!-- .advertisements -->
 
     <?php } ?>
 </div><!-- .container-advertisements-->
-
-<?php 
-    // Close connection
-    mysqli_close($db);
-?>
